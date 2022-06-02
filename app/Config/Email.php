354618d21c15a889a46a6fb74d98c7d791816560
helloworld-hkm <81341249+helloworld-hkm@users.noreply.github.com>
2022-06-02 12:37:43 +0700

    login awal
    sistem login admin awal

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromEmail;
+    public $fromEmail="hakimff622@gmail.com";
 
     /**
      * @var string
      */
-    public $fromName;
+    public $fromName="hakim";
 
     /**
      * @var string
